2570514054bc70502770641066c39d99f3c45821
theeramet0793 <theerametmetha@gmail.com>
2020-01-03 14:58:00 +0700

    commit-2

--- lab4_1.cpp
+++ lab4_1.cpp
@@ -1,3 +1,11 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
-int main() 
+int main(){
+double x,y;
+cout<<"Enter x : ";
+cin>>x;
+y=(3*(pow(x,3)))+2*(exp(1))+pow(2,(2*x)+1)+sqrt(pow(x,2)+1);
+cout<<"Result y = "<<y;
+
+} 
