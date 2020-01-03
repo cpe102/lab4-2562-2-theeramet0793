e245b827ee5b58b66d33267f749901bf6c495813
theeramet0793 <theerametmetha@gmail.com>
2020-01-03 15:07:59 +0700

    commit-1

--- lab4_2.cpp
+++ lab4_2.cpp
@@ -1,12 +1,25 @@
 #include<iostream>
+#include<cmath>
 
 using namespace std;
-
+double u,a,t,s;
 //Write function findDistance() here
+double findDistance(double u,double a,double t){
+  double s=(u*t)+(0.5)*a*pow(t,2);
+  return s;
+}
+
 
 int main(){
 
   //Calling findDistance() using test cases
+  cout<<"u : ";
+  cin>>u;
+  cout<<"a : ";
+  cin>>a;
+  cout<<"t : ";
+  cin>>t;
+  cout<<findDistance(u,a,t);
 
   return 0;
 }
