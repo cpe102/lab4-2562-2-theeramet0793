e1133971b7326ff2c6d8cd72540597130a42aab0
theeramet0793 <theerametmetha@gmail.com>
2020-01-03 15:19:14 +0700

    commit-1

--- lab4_3.cpp
+++ lab4_3.cpp
@@ -1,1 +1,28 @@
+#include<iostream>
+using namespace std;
+int x,n=2;
 
+int findDivisor(int x){
+    while(n!=1){
+        if(x%n==0){
+            return n;
+            n=1;
+        }
+        else
+        {
+            {n++;}
+        }
+        
+    }
+}
+
+main(){
+    p1:
+cout<<"Enter integer : ";
+cin>>x;
+if(x<2){
+    cout<<"Please enter more than 1 !!!!"<<"\n";
+    goto p1;
+}
+cout<<findDivisor(x);
+}
